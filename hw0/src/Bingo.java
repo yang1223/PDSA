7c0a7d1500931a21bbbafaee4176d3093139e468
yang1223 <whitesheep1223@gmail.com>
2016-02-29 01:41:34 +0800

    create hw0

---
+++ hw0/src/Bingo.java
@@ -0,0 +1,47 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class Bingo {
+
+    public static void main(String[] args) throws Exception {
+
+        // read file from args[0] in Java 7 style
+        try(BufferedReader br = new BufferedReader(new FileReader(args[0]))){
+            
+            // read a line and split by ','
+            String[] data = br.readLine().split(",");
+            
+            // store the first integer in variable stringCount (number of announced strings)
+            int stringCount = Integer.parseInt(data[0]);
+
+            // store the second integer in variable num (dimension of matrix: num * num)            
+            int num = Integer.parseInt(data[1]);
+
+            // initilization of a String array in Java
+            String[] announce = new String[stringCount];
+            String[][] matrix = new String[num][num];
+
+            // printf in Java (you should comment out or delete this in your final submission)
+            System.out.printf("number of announced strings: %d\ndimension of matrix: %d x %d\n", stringCount, num, num);
+
+            /*  now you can write your own solution to hw0
+             *  you can follow the instruction described below:
+             * 
+             *  1. read the rest content of the file
+             *  2. store the announce strings (2nd line of the file) in variable announce
+             *  3. store the matrix (from the 3rd line to the end of the file) in variable matrix
+             *  4. compare the matrix and announce strings (this is the tricky part)
+             *  5. output how many 'straight line' are there in the matrix
+             * 
+             *  [note]
+             *  you can use every data structure in standard Java packages (Java 8 supported)
+             *  the packages in stdlib.jar and algs4.jar are also available for you to use
+             *
+             *  [hint]
+             *  1. you should check whether Java pass the variable by references or by values.
+             *  2. some data structure such as HashSet, HashMap, Arrays, ArrayList, Vector are very
+             *     useful for solving problems. 
+             */
+        }
+    }
+}
