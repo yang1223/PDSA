c240b3d3c9948933f2107a608c501e4600a660e7
yang1223 <whitesheep1223@gmail.com>
2016-03-08 11:13:10 +0800

    asdfghjkl

--- hw1/src/Percolation.java
+++ hw1/src/Percolation.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 /**
  * Created by 其昌 on 2016/3/4.
@@ -20,19 +20,62 @@ public class Percolation {
             }
             br.close();
 
-            boolean[] isConnected = new boolean[lines.size()];
-            Arrays.fill(isConnected , false);
-
+            ArrayList<Grid> gridList = new ArrayList<Grid>();
             for(String line:lines){
-
+                String[] coordinate = line.split(",");
+                Grid grid = new Grid(Integer.parseInt(coordinate[0]),Integer.parseInt(coordinate[1]));
+                gridList.add(grid);
             }
+
+            System.out.println(gridList.size());
+
 
         } catch (IOException e){
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
-
     }
+
+    static class Grid {
+
+        private int x;
+        private int y;
+
+        Grid(int x , int y){
+            setX(x);
+            setY(y);
+        }
+
+        public boolean isConnected(Grid p){
+            if(x==p.getX()){
+                if(y-p.getY()==1||y-p.getY()==-1){
+                    return true;
+                }
+            } else if(y==p.getY()){
+                if(x-p.getX()==1||x-p.getX()==-1){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+
 
 }
