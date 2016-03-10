061ac909115044eb5fe605f63d1c663bf557ff68
yang1223 <whitesheep1223@gmail.com>
2016-03-11 01:13:24 +0800

    create hw2

---
+++ hw2/src/LabelCC.java
@@ -0,0 +1,9 @@
+/**
+ * Created by 其昌 on 2016/3/11.
+ */
+public class LabelCC {
+    public static void main(String[] args){
+        System.out.println("Hello HW2!");
+        System.out.println(args[0]);
+    }
+}
