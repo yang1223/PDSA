0f9c72a07e9b196cf80913d183fe5b25c54e0776
Yang, Chi-Chang <whitesheep1223@gmail.com>
2016-04-14 18:23:35 +0800

    finish hw5-1

--- hw5/src/MyConvexHull.java
+++ hw5/src/MyConvexHull.java
@@ -7,61 +7,55 @@ public class MyConvexHull {
 
     public static void main(String[] args) {
 
-        int num = 10;
-        Point2D[] points = new Point2D[num];
 
-//        Stack<Point2D> convexhull = new Stack<Point2D>();
+    }
 
-        for(int i = 0; i < num ; i++)
-            points[i] = new Point2D(StdRandom.uniform(),StdRandom.uniform());
+
+    public static int[] ConvexHullVertex(Point2D[] points) {
+
+
+        ArrayList<Point2D> array = new ArrayList<Point2D>();
+        for(Point2D p:points){
+            array.add(p);
+        }
 
         int min = findMinY(points);
 
-//        convexhull.push(points.get(min));
-
-
-        double[] angle = new double[num];
-
         Point2D start = points[min];
-        for(int i = 0 ; i < points.length ; i++){
-            Point2D p = points[i];
-            angle[i] = angleTo(start,p);
+//        Arrays.sort(points, start.ATAN2_ORDER);
+        quickSort(points, start.ATAN2_ORDER);
+
+        Stack<Point2D> convexhull = new Stack<Point2D>();
+        convexhull.push(points[0]);
+        convexhull.push(points[1]);
+        for(int i = 2 ; i < points.length ; i++){
+            Point2D b = convexhull.pop();
+            Point2D a = convexhull.pop();
+            Point2D c = points[i];
+            while (Point2D.ccw(a,b,c)!=1) {
+                b = a;
+                a = convexhull.pop();
+            }
+            convexhull.push(a);
+            convexhull.push(b);
+            convexhull.push(c);
         }
 
+        Stack<Point2D> reverse = new Stack<Point2D>();
+        while(!convexhull.isEmpty())
+            reverse.push(convexhull.pop());
+        convexhull = reverse;
 
-        Point2D p1 = new Point2D(0.5,0.1);
-        Point2D p2 = new Point2D(0.8,0.4);
-        Point2D p3 = new Point2D(0.6,0.7);
-//        p1.drawTo(p2);
-//        p2.drawTo(p3);
-//        System.out.println(Point2D.ccw(p1, p2, p3));
-//        System.out.println(Point2D.Y_ORDER.compare(p2,p1));
-
-
-
-
-        StdDraw.setCanvasSize(800, 800);
-        StdDraw.setXscale(-0.1, 1.1);
-        StdDraw.setYscale(-0.1, 1.1);
-
-        StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-
-        for(int i = 0 ; i < points.length ; i++){
-            Point2D p = points[i];
-
-            StdDraw.setPenRadius(.025);
-            p.draw();
-            StdDraw.text(p.x() + 0.015 , p.y() + 0.015 , i + " (" + String.format("%.0f" , (angle[i])*180/3.1416) + ")" );
+        Iterator<Point2D> iterator = convexhull.iterator();
+        int count = 0;
+        int[] index = new int[convexhull.size()];
+        while (iterator.hasNext()) {
+            Point2D p = iterator.next();
+            index[count++] = array.indexOf(p);
         }
-
-        StdDraw.setPenColor(StdDraw.MAGENTA);
-        points[min].draw();
-
-//        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-//        StdDraw.setPenRadius(.007);
-//        StdDraw.line(0.76,0.52,0.21,0.6);
-
+        return index;
     }
+
 
     public static int findMinY (Point2D[] points){
         int min = 0;
@@ -72,19 +66,35 @@ public class MyConvexHull {
         return min;
     }
 
-    public static double angleTo(Point2D p1 , Point2D p2) {
-        double dx = p2.x() - p1.x();
-        double dy = p2.y() - p1.y();
-        return Math.atan2(dy, dx);
+    public static void quickSort(Point2D[] array , Comparator<Point2D> c) {
+        quickSortKit(array , c , 0 , array.length-1);
     }
 
-    public static double[] quickSort(double[] array) {
-        // TODO: quickSort!
-        return array;
+    public static void quickSortKit(Point2D[] array , Comparator<Point2D> c , int start , int end) {
+
+        if(start >= end) return;
+        Point2D pivot = array[start];
+        int left = start;
+        int right = end+1;
+
+        while (true){
+            while (c.compare(pivot , array[++left]) == 1)
+                if (left == end) break;
+            while (c.compare(pivot , array[--right]) == -1)
+                if (right == start) break;
+
+            if(left >= right) break;
+
+            Point2D temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+        }
+        array[start] = array[right];
+        array[right] = pivot;
+
+        quickSortKit(array , c , start , right-1);
+        quickSortKit(array , c , right+1 , end);
+
     }
-
-
-
-
 
 }
