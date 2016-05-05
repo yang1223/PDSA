85bec4d8f1fcd1bd96358434b6cd30f2bfd1457e
Yang, Chi-Chang <whitesheep1223@gmail.com>
2016-05-05 15:35:51 +0800

    create hw8

---
+++ hw8/src/Clustering.java
@@ -0,0 +1,27 @@
+
+/**
+ * Created by Yang, Chi-Chang on 2016/5/5.
+ */
+public class Clustering {
+
+    double x;
+    double y;
+
+
+
+
+
+    public class Point {
+        double x;
+        double y;
+        Point(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+        public double getX(){return x;}
+        public double getY(){return y;}
+    }
+
+
+
+}
---
+++ hw8/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Yang, Chi-Chang on 2016/5/5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Clustering c = new Clustering();
+        System.out.println("Hi!");
+    }
+}
