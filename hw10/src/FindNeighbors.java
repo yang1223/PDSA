7a1a1a6a907a0df5a13bae7664445b5ffd818a99
Yang, Chi-Chang <whitesheep1223@gmail.com>
2016-05-25 22:14:50 +0800

    create hw10

---
+++ hw10/src/FindNeighbors.java
@@ -0,0 +1,21 @@
+public class FindNeighbors {
+
+    // DO NOT MODIFY THE CONSTRUCTOR! 
+    public FindNeighbors(){}
+
+    // TODO
+    // please use the Point2D from algs4.jar 
+    public void init(Point2D[] points){
+        return;
+    }
+
+    // TODO
+    // the result should be sorted accordingly to their distances to the query, from small to large
+    // please use the Point2D from algs4.jar 
+    public Point2D[] query(Point2D point, int k){
+        Point2D[] result = new Point2D[k];
+        return result;
+    }
+
+}
+
